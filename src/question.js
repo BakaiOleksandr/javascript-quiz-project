4368a9eee1f913364fe8694aef310d16b486c479
Oleksandr Bakai <saniabakay@gmail.com>
2025-07-08 20:47:07 +0200

    day2

--- src/question.js
+++ src/question.js
@@ -1,7 +1,16 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
-
-    // 2. shuffleChoices()
+    constructor(text, choices, answer, difficulty){
+        this.text = text;//text(string) of the question 
+        this.choices = choices;//array of strings   
+        this.answer =  answer;//(string)
+        this.difficulty = difficulty;//(number) from 1 to 3
+    }
+shuffleChoices() {
+    for (let i = this.choices.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+       let temp = this.choices[i];
+      this.choices[i] = this.choices[j];
+      this.choices[j] = temp; // обмен элементов
+    }
 }
+}//Questions........
--- src/quiz.js
+++ src/quiz.js
@@ -1,6 +1,70 @@
-class Quiz {
-    // YOUR CODE HERE:
-    //
+    class Quiz {
+        constructor(questions, timeLimit, timeRemaining){
+            this.questions = questions;
+            this.timeLimit = timeLimit;
+            this.timeRemaining = timeRemaining;
+            this.correctAnswers = 0;
+            this.currentQuestionIndex = 0;
+            }
+
+            getQuestion(){
+                return this.questions[this.currentQuestionIndex];
+            }
+
+            moveToNextQuestion(){
+                this.currentQuestionIndex++;
+            }
+
+            shuffleQuestions(){
+            for (let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+            let temp = this.questions[i];
+            this.questions[i] = this.questions[j];
+            this.questions[j] = temp; // обмен элементов
+            }
+            }
+
+
+            checkAnswer(answer) {
+            const currentQuestion = this.getQuestion();
+            if (answer === currentQuestion.answer) {
+            this.correctAnswers++;
+            return true;  
+            }
+            return false;
+            }
+
+
+            hasEnded(){
+                return this.currentQuestionIndex >= this.questions.length;
+            }
+
+
+            filterQuestionsByDifficulty(difficulty) {
+            if (typeof difficulty === 'number' && difficulty >= 1 && difficulty <= 3) {
+            const UpdateQuestions = [];
+            
+            for (let i = 0; i < this.questions.length; i++) {
+                let UpdateQuestionsIndex = this.questions[i];
+            if (UpdateQuestionsIndex.difficulty === difficulty) {
+                UpdateQuestions.push(UpdateQuestionsIndex);
+            }
+            }
+            this.questions = UpdateQuestions;
+            }
+            }
+
+            averageDifficulty(){
+                const sum = this.questions.reduce((accumulator,question)=>{
+                   return  accumulator + question.difficulty;
+                },0);
+                const average = sum/this.questions.length;
+                return average;
+            }
+
+
+        
+    }
     // 1. constructor (questions, timeLimit, timeRemaining)
 
     // 2. getQuestion()
@@ -12,4 +76,3 @@ class Quiz {
     // 5. checkAnswer(answer)
 
     // 6. hasEnded()
-}
